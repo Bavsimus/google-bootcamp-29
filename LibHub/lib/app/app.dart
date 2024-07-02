0a8fdd3ede324940a7db861765a96f0b4698850a
Talib Yesildal <82062115+Bavsimus@users.noreply.github.com>
2024-07-02 17:15:06 +0300

    Persona Library added
    - okunan kitaplar listesi eklendi.
    - kitap ekleme özelliğinin kontrolü için + butonu var ilerde kaldırılabilir.
    - kitapları aşağı scrolladığımız zaman your LibHub yazısının bulunduğu barın altında fazlalık gözüküyor onun düzeltilmesi gerek.

--- LibHub/lib/app/app.dart
+++ LibHub/lib/app/app.dart
@@ -1,9 +1,11 @@
+import 'package:libhub/ui/personalLib/personal_library_view.dart';
 import 'package:libhub/ui/registeration/login_view.dart';
 import 'package:libhub/ui/splash/splash_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 
 @StackedApp(routes: [
   MaterialRoute(page: LoginView),
-  MaterialRoute(page: SplashView, initial: true)
+  MaterialRoute(page: SplashView),
+  MaterialRoute(page: PersonalLibraryView, initial: true)
 ])
 class App {}
