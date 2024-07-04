f3a83c7e4f23f11cfa063c553c3f81f3484b4248
Talha Tashan <talhatashan@gmail.com>
2024-07-05 01:14:03 +0300

    home view added

--- LibHub/lib/home_ui/home_ui.dart
+++ LibHub/lib/home_ui/home_ui.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
-import "package:libhub/home_ui/home_screen.dart";
-import "package:libhub/ui/personalLib/personal_library_view.dart";
+
+import "package:libhub/home_ui/home_view_model.dart";
+
+import "package:stacked/stacked.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,55 +12,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  final List<Widget> _pages = [
-    HomeScreen(),
-    PersonalLibraryView(),
-    HomeScreen(),
-  ];
-  Widget _getPage(int index) {
-    switch (index) {
-      case 0:
-        return HomeScreen();
-      case 1:
-        return PersonalLibraryView();
-      case 2:
-        return HomeScreen();
-      default:
-        return HomeScreen();
-    }
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: _getPage(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.my_library_books_outlined),
-            label: 'Personal Library',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 33, 116, 93),
-        onTap: _onItemTapped,
-      ),
-    );
+    return ViewModelBuilder<HomeViewModel>.reactive(
+        viewModelBuilder: () => HomeViewModel(),
+        builder: (context, model, child) => Scaffold(
+              appBar: AppBar(),
+              body: model.getPage(model.selectedIndex),
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Homes',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.my_library_books_outlined),
+                    label: 'Personal Library',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Profile',
+                  ),
+                ],
+                currentIndex: model.selectedIndex,
+                selectedItemColor: Color.fromARGB(255, 33, 116, 93),
+                onTap: model.onItemTapped,
+              ),
+            ));
   }
 }
