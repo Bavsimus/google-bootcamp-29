bbaadbd8b9c44d4278ffe3622d44f4ab8fd3cfd6
Talib Yesildal <82062115+Bavsimus@users.noreply.github.com>
2024-07-18 00:11:51 +0300

    profile-page 1
    +LibHub 2 sekmeye bölündü(LibHub, Profile)
    +LibHub'a çalışan searchBar eklendi
    -Profile sayfası oluşturulacak

--- LibHub/lib/ui/personalLib/personal_library_view.dart
+++ LibHub/lib/ui/personalLib/personal_library_view.dart
@@ -7,114 +7,138 @@ class PersonalLibraryView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<PersonalLibraryViewModel>.reactive(
       viewModelBuilder: () => PersonalLibraryViewModel(),
-      builder: (context, model, child) => Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 8.0),
-              decoration: BoxDecoration(
-                color: Colors.blue, // Yazının arkaplan rengi
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20.0), // Alt sol köşe
-                  bottomRight: Radius.circular(20.0), // Alt sağ köşe
-                ),
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'Your LibHub',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.black, // Yazı rengi
-                    ),
-                  ),
-                  SizedBox(width: 8.0),
-                  Expanded(
-                    child: Container(
-                      height: 2.0,
-                      color: Colors.black,
-                      margin: EdgeInsets.symmetric(vertical: 8.0),
-                    ),
+      builder: (context, model, child) => DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: AppBar(
+              automaticallyImplyLeading: false,
+              flexibleSpace: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TabBar(
+                    indicatorColor: Color.fromARGB(
+                        255, 33, 116, 93), // Tab göstergesi rengi
+                    labelColor: Color.fromARGB(
+                        255, 33, 116, 93), // Seçili tab yazı rengi
+                    tabs: [
+                      Tab(text: 'LibHub'),
+                      Tab(text: 'Profile'),
+                    ],
                   ),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                color: Colors.grey[200], // Duman rengi arka plan
-                padding: EdgeInsets.all(8.0),
-                child: GridView.builder(
-                  itemCount: model.books.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Her sırada 3 kitap
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0,
-                    childAspectRatio: 0.5, // Yükseklik ve genişlik oranı
+          ),
+          body: TabBarView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      onChanged: model.updateSearchQuery,
+                      decoration: InputDecoration(
+                        hintText: 'Search...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        prefixIcon: Icon(Icons.search),
+                      ),
+                    ),
                   ),
-                  itemBuilder: (context, index) {
-                    final book = model.books[index];
-                    Color boxColor = index % 2 == 0 ? Colors.blue : Colors.red;
-
-                    return Container(
-                      padding: EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: boxColor, // Mavi veya kırmızı arka plan
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: Image.network(
-                                book.coverUrl,
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 4.0),
-                          Text(
-                            book.title,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.white, // Yazı rengi
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 2.0),
-                          Text(
-                            book.author,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white, // Yazı rengi
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
+                  Expanded(
+                    child: BooksGridView(books: model.books),
+                  ),
+                ],
               ),
+              Center(child: Text('This page is intentionally left blank')),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              model.addBook(
+                  'New Book', 'New Writer', 'https://example.com/new_book.jpg');
+            },
+            tooltip: 'Add book',
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BooksGridView extends StatelessWidget {
+  final List<Book> books;
+
+  BooksGridView({required this.books});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: GridView.builder(
+        itemCount: books.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 0.5,
+        ),
+        itemBuilder: (context, index) {
+          final book = books[index];
+          Color boxColor = index % 2 == 0
+              ? Colors.blueGrey
+              : Color.fromARGB(255, 33, 116, 93);
+
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(8.0),
+              color: boxColor,
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            model.addBook(
-                'New Book', 'New Writer', 'https://example.com/new_book.jpg');
-          },
-          tooltip: 'Add book',
-          child: Icon(Icons.add),
-        ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Image.network(
+                      book.coverUrl,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  book.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 2.0),
+                Text(
+                  book.author,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
--- LibHub/lib/ui/personalLib/personal_library_viewmodel.dart
+++ LibHub/lib/ui/personalLib/personal_library_viewmodel.dart
@@ -14,17 +14,24 @@ class PersonalLibraryViewModel extends BaseViewModel {
         'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/800px-The_Great_Gatsby_Cover_1925_Retouched.jpg'),
     Book('1984', 'George Orwell',
         'https://m.media-amazon.com/images/I/61NAx5pd6XL._AC_UF1000,1000_QL80_.jpg'),
-    Book('The Great Gatsby', 'F. Scott Fitzgerald',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/800px-The_Great_Gatsby_Cover_1925_Retouched.jpg'),
-    Book('The Great Gatsby', 'F. Scott Fitzgerald',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/800px-The_Great_Gatsby_Cover_1925_Retouched.jpg'),
-    Book('The Great Gatsby', 'F. Scott Fitzgerald',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/800px-The_Great_Gatsby_Cover_1925_Retouched.jpg'),
-    Book('The Great Gatsby', 'F. Scott Fitzgerald',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/800px-The_Great_Gatsby_Cover_1925_Retouched.jpg'),
+    Book('Moby Dick', 'Herman Melville',
+        'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781681778488/moby-dick-9781681778488_hr.jpg'),
   ];
 
-  List<Book> get books => _books;
+  String _searchQuery = '';
+
+  List<Book> get books => _searchQuery.isEmpty
+      ? _books
+      : _books
+          .where((book) =>
+              book.title.toLowerCase().contains(_searchQuery.toLowerCase()) ||
+              book.author.toLowerCase().contains(_searchQuery.toLowerCase()))
+          .toList();
+
+  void updateSearchQuery(String query) {
+    _searchQuery = query;
+    notifyListeners();
+  }
 
   void addBook(String title, String author, String coverUrl) {
     _books.add(Book(title, author, coverUrl));
